b8009cd50ceed6886a305e62a7e3637419db40b8
NidaaHamed <nidaasallam@gmail.com>
2024-03-24 19:59:49 +0200

    initial commit

---
+++ src/test/java/com/example/WordsGameApi/WordsGameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WordsGameApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsGameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
