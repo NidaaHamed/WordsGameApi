ed04e6fffa8c82125c7fa65c3a501da700b036ac
NidaaHamed <nidaasallam@gmail.com>
2024-03-28 23:54:58 +0200

    add cross origin

--- src/main/java/com/example/WordsGameApi/controllers/WordsGridController.java
+++ src/main/java/com/example/WordsGameApi/controllers/WordsGridController.java
@@ -2,10 +2,7 @@ package com.example.WordsGameApi.controllers;
 
 import com.example.WordsGameApi.servises.WordsGridService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,8 +13,9 @@ public class WordsGridController {
     @Autowired
     WordsGridService wordsGridService;
     @GetMapping("/wordgrid")
+    @CrossOrigin("http://localhost:1234")
     public String createWordGrid(@RequestParam int gridSize,@RequestParam String wordsList){
-        List<String> words = Arrays.asList(wordsList.split("-"));
+        List<String> words = Arrays.asList(wordsList.split(","));
         char[][] grid = wordsGridService.generateGrid(gridSize,words);
         String gridToString = "";
         for(int i=0 ; i<gridSize ; i++){
